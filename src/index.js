d11b5a79286d65511068d5d0fe51ec0c5d19298a
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-01-31 13:35:33 +0100

    display the scores fetched from the api to the dom

--- src/index.js
+++ src/index.js
@@ -4,14 +4,27 @@ const refreshBtn = document.querySelector('.refresh_btn');
 const userName = document.getElementById('name');
 const form = document.getElementById('score_form');
 const userScore = document.getElementById('score');
+const scoresContainer = document.querySelector('.score_board');
 const gameId = 'PA4nCuhNny5ySUjkcZyG';
 
 // url for fetching and creating my scores
 const SCORES_URL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`;
 
+// functionality for displaying the scores on the DOM
+const createLists = (scores) => {
+  scoresContainer.innerHTML = '';
+  scores.forEach((score) => {
+    const scoreContainer = document.createElement('h4');
+    scoreContainer.className = 'score';
+    scoreContainer.innerText = `${score.user}: ${score.score}`;
+    scoresContainer.appendChild(scoreContainer);
+  });
+};
+
 const fetchScores = async () => {
   const response = await fetch(`${SCORES_URL}`);
   const { result } = await response.json();
+  createLists(result);
   return result;
 };
 
