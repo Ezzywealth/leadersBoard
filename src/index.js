b1c19516a014526c650f9a64d1f106471f628524
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-01-31 13:13:27 +0100

    add functionality for fetching scores and submitting scores to the api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 import './style.css';
+
+const refreshBtn = document.querySelector('.refresh_btn');
+const userName = document.getElementById('name');
+const form = document.getElementById('score_form');
+const userScore = document.getElementById('score');
+const gameId = 'PA4nCuhNny5ySUjkcZyG';
+
+// url for fetching and creating my scores
+const SCORES_URL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`;
+
+const fetchScores = async () => {
+  const response = await fetch(`${SCORES_URL}`);
+  const { result } = await response.json();
+  return result;
+};
+
+const submitScore = async (details) => {
+  const response = await fetch(`${SCORES_URL}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(details),
+  });
+
+  const { result } = await response.json();
+  return result;
+};
+
+refreshBtn.addEventListener('click', () => {
+  fetchScores(SCORES_URL);
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const object = {
+    user: userName.value,
+    score: +userScore.value,
+  };
+  submitScore(object);
+  form.reset();
+});
